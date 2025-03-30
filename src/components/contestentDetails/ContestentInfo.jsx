e22c10e9efa3cc8c3c73fdb89c35752b2e6d5a33
Sumitraj00736 <sumitraj00736@gmail.com>
2025-03-30 09:53:09 +0530

    Feat: Hide Pre-Mature Updates(voteCard )

--- src/components/contestentDetails/ContestentInfo.jsx
+++ src/components/contestentDetails/ContestentInfo.jsx
@@ -371,20 +371,20 @@ export default function VotingComponent() {
                       </div>
                     ) : null}
                     <ProfileCard />
-                    {(selectedCountry?.cc === "np"|| selectedCountry?.cc === "in")  && (
+                    {(selectedCountry?.cc === "np")  && (
                       <>
                         <button
                           onClick={handleQrClick}
-                          className="w-28 md:w-32 mt-6 ml-2 py-3 border border-white text-white text-[7px] rounded-lg hover:bg-white hover:text-[#0A1128] transition duration-300"
+                          className="w-56 md:w-64 mt-6 ml-2 py-3 border border-white text-white text-[7px] rounded-lg hover:bg-white hover:text-[#0A1128] transition duration-300"
                         >
                           Generate QR to Vote
                         </button>
-                        <button
+                        {/* <button
                           onClick={handleVoteCard}
                           className="w-28 md:w-32 mt-6 ml-2 py-3 border border-white text-white text-[7px] rounded-lg hover:bg-white hover:text-[#0A1128] transition duration-300"
                         >
                           Show Vote Card
-                        </button>
+                        </button> */}
                       </>
                     )}
                   </div>
