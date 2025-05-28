69bd960d1763a8f424c37ac98bc56d36eb700534
Pramit Bakhrel <pramit.bakhrel@remitpe.com>
2025-05-28 15:14:06 +0545

    chore: dependencies updated

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,39 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
-import { VitePWA } from 'vite-plugin-pwa' 
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({
-    registerType: 'autoUpdate',
-    includeAssets: ['favicon.ico'],
-    manifest: {
-      name: 'ZeenoPay',
-      short_name: 'ZeenoPay',
-      start_url: '/',
-      display: 'standalone',
-       "theme_color": "#000B44",
-    "background_color": "#00035c",
-      description: 'ZeenoPay is a platform that allows users to make payments for events and services using QR codes.',
-      icons: [
-        { src: '/assets/favicon.png', sizes: '192x192', type: 'image/png' },
-        { src: '/assets/favicon.png', sizes: '512x512', type: 'image/png' }
-      ]
+  plugins: [
+    react(), 
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico'],
+      manifest: {
+        name: 'ZeenoPay',
+        short_name: 'ZeenoPay',
+        start_url: '/',
+        display: 'standalone',
+        "theme_color": "#000B44",
+        "background_color": "#00035c",
+        description: 'ZeenoPay is a platform that allows users to make payments for events and services using QR codes.',
+        icons: [
+          { src: '/assets/favicon.png', sizes: '192x192', type: 'image/png' },
+          { src: '/assets/favicon.png', sizes: '512x512', type: 'image/png' }
+        ]
+      }
+    })
+  ],
+  build: {
+    rollupOptions: {
+      external: [],
+      output: {
+        globals: {}
+      }
     }
-  })],
+  },
+  optimizeDeps: {
+    include: ['html-to-image']
+  }
 });
